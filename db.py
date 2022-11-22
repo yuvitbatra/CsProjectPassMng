e6e7c85703fd16d8316c14925f70df4ee70fa2af
Aditya Gautam <66688960+AdiBro027@users.noreply.github.com>
2022-11-22 22:31:37 +0530

    Add files via upload

---
+++ db.py
@@ -0,0 +1,13 @@
+import os
+os.system("pip install -r requirements.txt")
+
+import pymysql
+
+conn = pymysql.connect(host="localhost",user="root",password="amity")
+c = conn.cursor()
+c.execute("create database adityagautampassmng25346")
+conn.commit()
+conn.select_db("adityagautampassmng25346")
+c.execute("create table passlog(sno int not null, softname varchar(100),password varchar(512),emailId varchar(200), deviation bigint)")
+
+
